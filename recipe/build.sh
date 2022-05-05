a91f8279ed63038ac08dc958c56ec18c135a221b
leej3 <leej3@quansight.com>
2022-05-05 05:10:58 -0400

    tidy build.sh and copy pixbuf loaders

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,7 @@ configure_args=(
     --disable-static
     --enable-pixbuf-loader=yes
     --enable-introspection=yes
+    --without-ran
 )
 
 if [[ $target_platform == osx-* ]] ; then
@@ -18,47 +19,16 @@ if [[ $target_platform == osx-* ]] ; then
   export LDFLAGS="$LDFLAGS -lobjc"
 fi
 
+#export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${CONDA_BUILD_SYSROOT}/usr/lib/pkgconfig:${CONDA_BUILD_SYSROOT}/usr/lib64/pkgconfig:${CONDA_BUILD_SYSROOT}/usr/share/pkgconfig:${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/lib64/pkgconfig"
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
-  (
-    unset CARGO_BUILD_TARGET
-    mkdir -p native-build
-    pushd native-build
-
-    export CC=$CC_FOR_BUILD
-    export AR=($CC_FOR_BUILD -print-prog-name=ar)
-    export NM=($CC_FOR_BUILD -print-prog-name=nm)
-    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-    export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
-
-    # Unset them as we're ok with builds that are either slow or non-portable
-    unset CFLAGS
-    unset CPPFLAGS
-    export host_alias=$build_alias
-    export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
-
-    ../configure --prefix=$BUILD_PREFIX "${configure_args[@]}"
-
-    # This script would generate the functions.txt and dump.xml and save them
-    # This is loaded in the native build. We assume that the functions exported
-    # by glib are the same for the native and cross builds
-    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
-    make -j${CPU_COUNT}
-    make install
-    popd
-  )
-  export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
-fi
-
-export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
-
+export PKG_CONFIG_LIBDIR=${PREFIX}/lib
 export RUST_TARGET=$CARGO_BUILD_TARGET
 unset CARGO_BUILD_TARGET
 
-./configure --prefix=$PREFIX "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+./configure --prefix=$PREFIX "${configure_args[@]}"
 make -j$CPU_COUNT
 make install
+# It's not clear why the following is not installed by the build system
+cp gdk-pixbuf-loader/.libs/libpixbufloader-svg.so $PREFIX/lib/gdk-pixbuf-2.0/2.10.0/loaders
 
 rm -rf $PREFIX/share/gtk-doc
