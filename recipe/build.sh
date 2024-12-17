d4a31d1351b8f9f71066d4daa988d7d2a7df0502
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2024-12-17 16:08:02 +0200

    librsvg 2.56.3 (#9)
    * Use setuptools <74
    
    * Use vs2022
    
    * Clean up the recipe
    
    * Update host pinnings, fix rust compiler in build
    
    * Set pin_compatible for libiconv in run
    
    * Remove libdeflate and zlib from host and run as the library relies on rust crates
    
    * Use zlib on osx
    
    * Do not use optional deps on win
    
    * Remove libiconv
    
    * Remove obsolete code
    
    * Remove libiconv from run
    
    * Add libiconv on osx
    
    * Check the downstream packages
    
    * Disable downstream ffmpeg
    
    * Disable downstreams
    
    * Remove downstreams
    
    * Remove staging channels

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,38 +4,17 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
-export SED=sed
-export EGREP="grep -E"
-export FGREP="grep -F"
-export GREP="grep"
-export MKDIR="mkdir"
-export MKDIR_P="mkdir -p"
-
-where=$(which "install" 2>/dev/null || true)
-
-if [ -n "${where}" ]; then
-  ln -s -f ${where} ./install
-fi
-
-export INSTALL="install"
-
-if [[ ${target_platform} == linux-ppc64le ]]; then
-  # there are issues with CDTs and there HOST name ...
-  pushd "${BUILD_PREFIX}"
-  cp -Rn powerpc64le-conda-linux-gnu/* powerpc64le-conda_cos7-linux-gnu/. || true
-  cp -Rn powerpc64le-conda_cos7-linux-gnu/* powerpc64le-conda-linux-gnu/. || true
-  popd
-fi
-
-
 export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH_FOR_BUILD}:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
 
 if [ -n "${XDG_DATA_DIRS}" ]; then
   export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share:$BUILD_PREFIX/share
 else
   export XDG_DATA_DIRS=$PREFIX/share:$BUILD_PREFIX/share
 fi
+
+# https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 configure_args=(
     --disable-Bsymbolic
